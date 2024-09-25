d8b15bbc67fb563d5d67c7b1f4fb61459a09e291
ljh0401 <dlwnsghks0401@gmail.com>
2024-09-25 17:25:43 +0900

    범인 색출 프로그램 v1.1

--- who.py
+++ who.py
@@ -1,25 +1,30 @@
 from datetime import datetime, timedelta
 import requests
+class Who:
 
-def get_commits(author, branch='main'):
-    yesterday = (datetime.now() - timedelta(days=1)).isoformat()
-    url = f'https://api.github.com/repos/seohye-ki/morning-in-algo/commits'
-    params = {'sha': branch, 'author': author, 'since' : yesterday}
-    
-    response = requests.get(url, params=params)
-    
-    if response.status_code == 200:
-        commits = response.json()
-        if commits:
-            print(author,"통과")
-        else :
-            print(author,"반성하세요")
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+    def __init__(self):
 
-def main():
-    authors = {'seohye-ki','ljh0401','pasongvan', 'isemae', 'criti0728'}
-    for author in authors:
-        get_commits(author)
+        authors = ['seohye-ki','ljh0401','pasongvan', 'isemae', 'criti0728']
 
-main()
+        for author in authors:
+            self.get_commits(author)
+
+    def get_commits(self, author, branch='main'):
+        yesterday = (datetime.now() - timedelta(days=1)).isoformat()
+        url = 'https://api.github.com/repos/seohye-ki/morning-in-algo/commits'
+        params = {'sha': branch, 'author': author, 'since' : yesterday}
+        print(url, params)
+
+        response = requests.get(url, params=params)
+        
+        if response.status_code == 200:
+            commits = response.json()
+            if commits:
+                print(author,"통과")
+            else :
+                print(author,"반성하세요")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+
+Who()
