ed0f14f354afdf9fc30df7b32c78b598282cc320
isemae <foxlis97@naver.com>
2024-09-29 17:04:53 +0900

    [BOJ]25192 / 인사성 밝은 곰곰이 / 실버4 / jiwoonlee

---
+++ jiwoonlee/Week1/src/BOJ_S4_25192_인사성_밝은_곰곰이/Main.java
@@ -0,0 +1,26 @@
+package BOJ_S4_인사성_밝은_곰곰이_25192;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Set<String> set = new HashSet<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			String chat = br.readLine();
+			if (chat.equals("ENTER")) {
+				set.clear();
+			} else if (set.add(chat)) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
